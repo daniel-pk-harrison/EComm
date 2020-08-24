2328af06b8e0f853594588ee9684fb0e513d9372
Daniel Harrison <daniel.pk.harrison@gmail.com>
2020-08-24 19:31:07 -0400

    added data context

---
+++ EComm.DataAccess/ECommDataContext.cs
@@ -0,0 +1,27 @@
+﻿using EComm.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EComm.DataAccess
+{
+    public class ECommDataContext : DbContext
+    {
+        public DbSet<Customer> CUstomers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+
+        public ECommDataContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                entity.Relational().TableName = entity.ClrType.Name;
+            }
+        }
+    }
+}
--- EComm.Web/Startup.cs
+++ EComm.Web/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EComm.DataAccess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -31,6 +33,7 @@ namespace EComm.Web
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddDbContext<ECommDataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ECommConnection")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
