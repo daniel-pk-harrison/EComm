6e399d2024d8af43f17f3835f5dcced6006dcefe
Daniel Harrison <daniel.pk.harrison@gmail.com>
2020-08-26 17:52:00 -0400

    new product controller and detail view

---
+++ EComm.Web/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EComm.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EComm.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ECommDataContext _dataContext;
+
+        public ProductController(ECommDataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public IActionResult Detail(int id)
+        {
+            var product = _dataContext.Products.Include(p => p.Supplier).SingleOrDefault(p => p.Id == id);
+            return View(product);
+        }
+    }
+}
