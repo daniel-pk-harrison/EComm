41af991a8e038b5147b0ce1aa2f4f71e44ce415a
Daniel Harrison <daniel.pk.harrison@gmail.com>
2020-08-26 19:47:44 -0400

    added productviewmodel used to edit the product, updated edit action

--- EComm.Web/Controllers/ProductController.cs
+++ EComm.Web/Controllers/ProductController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EComm.DataAccess;
+using EComm.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace EComm.Web.Controllers
@@ -31,7 +33,25 @@ namespace EComm.Web.Controllers
             var product = _dataContext.Products.SingleOrDefault(p => p.Id == id);
             //linq doesnt find a product return 404
             if (product == null) return NotFound();
-            return View(product);
+            //build list of suppliers
+            var suppliers = _dataContext.Suppliers.ToList();
+            //create a productviewmodel using our found product's properties
+            var pvm = new ProductEditViewModel
+            {
+                Id = product.Id,
+                ProductName = product.ProductName,
+                UnitPrice = product.UnitPrice,
+                Package = product.Package,
+                IsDiscontinued = product.IsDiscontinued,
+                SupplierId = product.SupplierId,
+                //create our list of <select> options using the suppliers list
+                Suppliers = suppliers.Select(s => new SelectListItem
+                {
+                    Text = s.CompanyName,
+                    Value = s.Id.ToString()
+                }).ToList()
+            };
+            return View(pvm);
         }
     }
 }
---
+++ EComm.Web/ViewModels/ProductEditViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EComm.Web.ViewModels
+{
+    public class ProductEditViewModel
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public string Package { get; set; }
+        public bool IsDiscontinued { get; set; }
+        public int SupplierId { get; set; }
+
+        public List<SelectListItem> Suppliers { get; set; }
+    }
+}
