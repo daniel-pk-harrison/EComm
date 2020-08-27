5991348aba833a9cf6506fa30d480f11829334c0
Daniel Harrison <daniel.pk.harrison@gmail.com>
2020-08-26 20:06:39 -0400

    added data annotations to producteditviewmodel

--- EComm.Web/ViewModels/ProductEditViewModel.cs
+++ EComm.Web/ViewModels/ProductEditViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,10 +10,17 @@ namespace EComm.Web.ViewModels
     public class ProductEditViewModel
     {
         public int Id { get; set; }
+        [Display(Name = "Product Name")]
+        [Required(ErrorMessage = "Product Name is required")]
         public string ProductName { get; set; }
+        [Display(Name = "Unit Price")]
+        [Required(ErrorMessage = "Unit Price is required")]
+        [Range(1.00, 500.00)]
         public decimal? UnitPrice { get; set; }
         public string Package { get; set; }
+        [Display(Name = "Is Discotinued?")]
         public bool IsDiscontinued { get; set; }
+        [Display(Name = "Supplier")]
         public int SupplierId { get; set; }
 
         public List<SelectListItem> Suppliers { get; set; }
